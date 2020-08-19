0aeaf74dce1d92ea3cb51149b8d8bec05f1eabb6
Paulina Guerrero <paaulinagp@gmail.com>
2020-08-19 18:54:03 -0500

    feat: ejemplos de pipes para strings

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'pipes';
+  nombre: string = 'Paulina Guerrero';
+  arreglo = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 }
