5805b537ea0c2a92b623824dca58ff4ed7bee6c5
Paulina Guerrero <paaulinagp@gmail.com>
2020-08-19 20:04:01 -0500

    feat: agregando ejemplos de los pipes:  number, currency, percent y json

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,4 +8,17 @@ import { Component } from '@angular/core';
 export class AppComponent {
   nombre: string = 'Paulina Guerrero';
   arreglo = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  PI: number = Math.PI;
+  porcentaje: number = 0.234;
+  salario: number = 1234.5;
+
+  heroe = {
+    nombre: 'Logan',
+    clave: 'Wolverine',
+    edad: 500,
+    direccion: {
+      calle: 'Primera',
+      casa: '20',
+    },
+  };
 }
